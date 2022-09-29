763792e6a5e8786acd142c6c1ac0bca440d1a5a8
poipiii <nanosonico1@gmail.com>
2022-09-29 10:35:59 +0800

    [feat] add enter field and cond render for todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Todobar } from './components/todobar'
+import Box from '@mui/material/Box';
+import { Stack } from '@mui/material';
+import Todoenter from './components/todoenter';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <Todobar></Todobar>
+        <Box>
+          <Todoenter></Todoenter>
+        </Box>
+        <Stack direction="column" spacing={2}> 
+          <Todobar todo_id={1} todo_txt='Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..' todo_status='notdone'></Todobar>
+          <Todobar todo_id={2} todo_txt='Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..' todo_status='done'></Todobar>
+          </Stack>
       </div>
     </div>
   )
--- src/components/todobar.tsx
+++ src/components/todobar.tsx
@@ -4,26 +4,35 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
-
-type todo = {
+import RestoreIcon from '@mui/icons-material/Restore';
+interface todo  {
     todo_id:number,
     todo_txt:string,
     todo_status:string
 }
 
-export const Todobar = (prop:todo) => {
+export const Todobar = ({todo_id,todo_txt,todo_status}:todo) => {
   return (
     <div>
         <Box sx={{display:'inline-flex',boxShadow:1,borderRadius:1}}>
             <Typography sx={{paddingY:'8px' ,paddingX:'15px'}}>
-                this is a test of text in a todobar 
+                {todo_txt}
             </Typography>
             <IconButton aria-label='delete' color='error'>
                 <DeleteIcon></DeleteIcon>
             </IconButton>
-            <IconButton aria-label='check' color='success'>
+            {todo_status==='done'?
+             <IconButton aria-label='check' color='warning'>
+                <RestoreIcon></RestoreIcon>
+            </IconButton>
+       
+            :
+              <IconButton aria-label='check' color='success'>
                 <CheckIcon></CheckIcon>
             </IconButton>
+           
+        }
+           
             
 
         </Box>
